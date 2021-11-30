f46c3da2cca6b94e00fef4686a4266ae2f1f86e6
Anirudh906 <40207836+Anirudh906@users.noreply.github.com>
2021-11-30 14:48:52 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -27,8 +27,15 @@ mongoose
  app.use('/api/profile', require('./api/profile'));
  app.use('/api/auth', require('./api/auth'));
 
- app.get('/', (req, res) => res.send('Server Running !'));
 
+
+ if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => { 
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+ }
  
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
